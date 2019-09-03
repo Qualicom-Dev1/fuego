40ed0d8469c44d321b250a963655ba59e2e7e36c
qualicomWendy <wendy@qualicom-conseil.fr>
2019-09-03 14:30:41 +0200

    wendy0309

--- public/assets/js/modifier_client.js
+++ public/assets/js/modifier_client.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
 
     $('.btn_modifier_client').click((event) => {
-
         var element = document.getElementById("icon_modif");
-        element.classList.remove("fa-pen");
+        
+    element.classList.remove("fa-pen");
         element.classList.add("fa-check");
+
 
     });
 
-
+    element.classList.remove("fa-check");
+    element.classList.add("fa-pen");
 });
