c0186dea070854cae55864d62f51b0646acb5cb5
JOULKVA <johan@qualicom-conseil.fr>
2019-09-16 15:30:55 +0200

    Route marketing

---
+++ routes/marketing.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/' ,(req, res, next) => {
+    res.render('/marketing/gestion', { extractStyles: true, title: 'Menu'});
+});
+
+router.get('/besoins-commandes' ,(req, res, next) => {
+    res.render('/marketing/besoins_commandes', { extractStyles: true, title: 'Menu'});
+});
+
+router.get('/suivi-commandes' ,(req, res, next) => {
+    res.render('/marketing/suivi_commandes', { extractStyles: true, title: 'Menu'});
+});
+
+
+module.exports = router;
