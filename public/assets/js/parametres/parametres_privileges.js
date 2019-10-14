7bf5c93e953acaa5a3747e76da863577e1a3fb73
JOULKVA <johan@qualicom-conseil.fr>
2019-10-14 15:08:20 +0200

    test

--- public/assets/js/parametres/parametres_privileges.js
+++ public/assets/js/parametres/parametres_privileges.js
@@ -1,6 +1,15 @@
 $(document).ready(() => {
      
+    $('.fa-times').hide()
+    $('.fa-check').hide()
+
     $('.role_item').click( event => {
+
+        $('.fa-times').show()
+        $('.fa-check').show()
+
+        $('.role_item').removeClass('active')
+        $(event.currentTarget).addClass('active')
         
         $.ajax({
             url: '/parametres/privileges/get-privileges-role',
@@ -26,4 +35,34 @@ $(document).ready(() => {
         }
     })
 
+
+    $('.fa-times').click( () => {
+        $('.privilege_item').removeClass('active')
+        $('.role_item').removeClass('active')
+    })
+
+    $('.fa-check').click( () => {
+        let idPrivileges = []
+        $('.privilege_item.active').each((index, element) => {
+            idPrivileges.push(element.id.split('_')[1])
+        })
+        let idRole = $('.role_item.active')[0].id.split('_')[1]
+
+        let data = {
+            privileges: idPrivileges,
+            role: idRole
+        }
+        $.ajax({
+            url: '/parametres/privileges/set-privileges-role',
+            method: 'POST',
+            data: data
+        }).then(data => {
+            $('.privilege_item').removeClass('active')
+            $('.role_item').removeClass('active')
+            $('.fa-times').hide()
+            $('.fa-check').hide()
+        });
+
+
+    })
 });
