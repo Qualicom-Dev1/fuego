b79640113de1caca9f642ae42f2b4f0ab0142180
Wendy <wendy@qualicom-conseil.fr>
2019-10-14 16:45:34 +0200

    onglet poses

---
+++ routes/poses.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/' ,(req, res, next) => {
+    res.render('poses/poses_dashboard', { extractStyles: true, title: 'Menu', options_top_bar: 'poses'});
+});
+
+
+router.get('/poses_dashboard' ,(req, res, next) => {
+    res.render('poses/tableau-de-bord', { extractStyles: true, title: 'Menu', options_top_bar: 'poses'});
+});
+
+
+module.exports = router;
