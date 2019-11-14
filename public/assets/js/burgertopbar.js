e398299aa70c9480dd15600ab4f1b62fd5873248
Wendy <wendy@qualicom-conseil.fr>
2019-11-14 11:20:36 +0100

    Version ALPHA 1.0
    responsive v1

---
+++ public/assets/js/burgertopbar.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('#burger_topbar').click((event) => {
+            
+        if($('#burger_topbar').hasClass('activeburger')){
+            $('#burger_topbar').removeClass('activeburger');
+
+    } else {
+                    $('#burger_topbar').addClass('activeburger');
+    }
+    });
+    
+    
+});
