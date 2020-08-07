80838ba6508a44d157c6b7c9637909cb2a35dc29
Rémi Courvoisier <remi.courvoisier94@gmail.com>
2020-08-07 16:01:15 +0200

    modification des agendas manager/telepro/vendeurs

--- public/assets/js/teleconseiller/telec_agenda.js
+++ public/assets/js/teleconseiller/telec_agenda.js
@@ -1,3 +1,5 @@
+let tippyInstance = undefined
+
 $( document).ready(() => {
 
     let calendarEl = document.getElementById('calendar')
@@ -18,7 +20,8 @@ $( document).ready(() => {
             month:    'Mois',
             week:     'Semaine',
             day:      'Jour'
-          },
+        },
+        eventLimit : 6,
         eventSources: [
             {
               url: '/teleconseiller/event',
@@ -26,7 +29,7 @@ $( document).ready(() => {
               failure: function() {
                 alert('there was an error while fetching events!');
               },
-              textColor: 'black'
+              color : '#f2b42c'
             },{
               url: '/teleconseiller/abs',
               method: 'POST',
@@ -36,7 +39,49 @@ $( document).ready(() => {
               color: 'red',
               textColor: 'black'
             }
-        ]
+        ],
+        eventRender : function(info) {
+          try {                
+              if(['BADGING', 'PARRAINAGE', 'PERSO'].includes(info.event.extendedProps.source)) {
+                  const title = info.event.title
+                  const prefix = info.event.extendedProps.source
+
+                  info.el.querySelector('.fc-title').textContent = `${prefix} ${title}`
+              }
+              
+              tippy(info.el, {
+                  theme: 'light',
+                  content : info.event.extendedProps.tooltip
+              })
+          }
+          catch(e) {
+              console.error(e)
+          }
+      },
+      eventClick : function(info) {
+          let same = false
+
+          // retire l'instance en cours
+          if(tippyInstance) {
+              same = tippyInstance.reference == info.el
+
+              tippyInstance.unmount()
+              tippyInstance.destroy()
+
+              document.querySelectorAll('div[id^=tippy]').forEach(elt => elt.parentNode.removeChild(elt))
+
+              tippyInstance = undefined
+          }
+          
+          // si click sur autre event, on affiche le tooltip
+          if(!same) {
+              tippyInstance = tippy(info.el, {
+                  theme: 'light',
+                  content : info.event.extendedProps.tooltip
+              })
+              tippyInstance.show()
+          }
+      }
     })
     calendar.render()
 })
