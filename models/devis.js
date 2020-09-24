5e4f4ed687c56c954c1d9a9669d935f36d3abbd6
Rémi Courvoisier <remi.courvoisier94@gmail.com>
2020-09-24 11:50:39 +0200

    ajout des models des nouvelles tables pour la facturation TMK

---
+++ models/devis.js
@@ -0,0 +1,106 @@
+module.exports = (sequelize, DataTypes) => {
+    const Devis = sequelize.define('Devis', 
+        {
+            refDevis : {
+                type : DataTypes.STRING(100),
+                allowNull : false,
+                validate : {
+                    len : {
+                        args : [3, 100],
+                        msg : "Le numéro de référence du devis doit être compris entre 3 et 100 caractères."
+                    },
+                    notNull : {
+                        msg : "Le numéro de référence du devis doit être indiqué."
+                    }
+                }
+            },
+            idPrestation : {
+                type : DataTypes.INTEGER,
+                allowNull : false,
+                validate : {
+                    notNull : {
+                        msg : "Le devis doit être lié à une prestation."
+                    }
+                }
+            },
+            isValidated : {
+                type : DataTypes.BOOLEAN,
+                allowNull : false,
+                defaultValue : 0
+            },
+            tva : {
+                type : DataTypes.FLOAT,
+                allowNull : false,
+                defaultValue : 20,
+                validate : {
+                    min : {
+                        args : 2.1,
+                        msg : "Le taux minimal de TVA doit être de 2,1%."
+                    },
+                    notNull : {
+                        msg : "Le taux de TVA doit être indiqué."
+                    }
+                }
+            },
+            remise : {
+                type : DataTypes.FLOAT,
+                allowNull : false,
+                defaultValue : 0,
+                validate : {
+                    min : {
+                        args : 0,
+                        msg : "La remise ne peut pas être négative."
+                    },
+                    notNull : {
+                        msg : "Une remise nulle doit être mise à 0 (zéro)."
+                    }
+                }
+            },
+            prixHT : {
+                type : DataTypes.FLOAT,
+                allowNull : false,
+                defaultValue : 0,
+                validate : {
+                    min : {
+                        args : 0,
+                        msg : "Le prix doit forcément être positif."
+                    },
+                    notNull : {
+                        msg : "Le prix HT doit être renseigné."
+                    }
+                }
+            },
+            prixTTC : {
+                type : DataTypes.FLOAT,
+                allowNull : false,
+                defaultValue : 0,
+                validate : {
+                    min : {
+                        args : 0,
+                        msg : "Le prix doit forcément être positif."
+                    },
+                    notNull : {
+                        msg : "Le prix TTC doit être renseigné."
+                    }
+                }
+            },
+            isCanceled : {
+                type : DataTypes.BOOLEAN,
+                allowNull : false,
+                defaultValue : 0
+            }
+        }, 
+        {
+            name : {
+                singular : 'Devis',
+                plural : 'Devis'
+            }
+        }
+    )
+
+    Devis.associate = models => {
+
+    }
+
+    return Devis
+}
---
+++ models/factures.js
@@ -0,0 +1,165 @@
+const moment = require('moment')
+
+module.exports = (sequelize, DataTypes) => {
+    const Facture = sequelize.define('Facture', 
+        {
+            refFacture : {
+                type : DataTypes.STRING(100),
+                allowNull : false,
+                validate : {
+                    len : {
+                        args : [3, 100],
+                        msg : "Le numéro de référence de la facture doit être compris entre 3 et 100 caractères."
+                    },
+                    notNull : {
+                        msg : "Le numéro de référence de la facture doit être indiqué."
+                    }
+                }
+            },
+            idDevis : {
+                type : DataTypes.INTEGER,
+                allowNull : true,
+                defaultValue : null
+            },
+            idPrestation : {
+                type : DataTypes.INTEGER,
+                allowNull: false,
+                validate : {
+                    notNull : {
+                        msg : "La facture doit être liée à une prestation."
+                    }
+                }
+            },
+            dateEmission : {
+                type : DataTypes.DATE,
+                allowNull : false,
+                defaultValue : sequelize.literal('CURRENT_TIMESTAMP'),
+                get() {
+                    return moment(this.getDataValue('dateEmission')).format('DD/MM/YYYY HH:mm')
+                },
+                set(value) {
+                    this.setDataValue('dateEmission', moment(value, 'DD/MM/YYYY HH:mm').format('YYYY-MM-DD HH:mm:00'))
+                }
+            },
+            dateEcheance : {
+                type : DataTypes.DATE,
+                allowNull : false,
+                validate : {
+                    notNull : {
+                        msg : "La date d'échéance de paiement doit être indiquée."
+                    }
+                },
+                get() {
+                    return moment(this.getDataValue('dateEcheance')).format('DD/MM/YYYY')
+                },
+                set(value) {
+                    this.setDataValue('dateEcheance', moment(value, 'DD/MM/YYYY').format('YYYY-MM-DD'))
+                }
+            },
+            type : {
+                type : DataTypes.ENUM('acompte','avoir','solde'),
+                allowNull : false,
+                defaultValue : 'solde',
+                validate : {
+                    isIn : {
+                        args : [['acompte','avoir','solde']],
+                        msg : "Le type de facture doit être soit d'acompte, soit d'avoir soit du solde."
+                    },
+                    notNull : {
+                        msg : "Le type de facture doit être indiqué."
+                    }
+                }
+            },
+            tva : {
+                type : DataTypes.FLOAT,
+                allowNull : false,
+                defaultValue : 20,
+                validate : {
+                    min : {
+                        args : 2.1,
+                        msg : "Le taux minimal de TVA doit être de 2,1%."
+                    },
+                    notNull : {
+                        msg : "Le taux de TVA doit être indiqué."
+                    }
+                }
+            },
+            remise : {
+                type : DataTypes.FLOAT,
+                allowNull : false,
+                defaultValue : 0,
+                validate : {
+                    min : {
+                        args : 0,
+                        msg : "La remise ne peut pas être négative."
+                    },
+                    notNull : {
+                        msg : "Une remise nulle doit être mise à 0 (zéro)."
+                    }
+                }
+            },
+            idTypePaiement : {
+                type : DataTypes.INTEGER,
+                allowNull : false,
+                validate : {
+                    notNull : {
+                        msg : "Un type de paiement doit être choisi."
+                    }
+                }
+            },
+            datePaiement : {
+                type : DataTypes.DATE,
+                allowNull : true,
+                defaultValue : null,
+                get() {
+                    return this.getDataValue('datePaiement') !== null ? moment(this.getDataValue('datePaiement')).format('DD/MM/YYYY') : null
+                },
+                set(value) {
+                    this.setDataValue('datePaiement', value !== null ? moment(value, 'DD/MM/YYYY').format('YYYY-MM-DD') : null)
+                }
+            },
+            prixHT : {
+                type : DataTypes.FLOAT,
+                allowNull : false,
+                defaultValue : 0,
+                validate : {
+                    min : {
+                        args : 0,
+                        msg : "Le prix doit forcément être positif."
+                    },
+                    notNull : {
+                        msg : "Le prix HT doit être renseigné."
+                    }
+                }
+            },
+            prixTTC : {
+                type : DataTypes.FLOAT,
+                allowNull : false,
+                defaultValue : 0,
+                validate : {
+                    min : {
+                        args : 0,
+                        msg : "Le prix doit forcément être positif."
+                    },
+                    notNull : {
+                        msg : "Le prix TTC doit être renseigné."
+                    }
+                }
+            },
+            isCanceled : {
+                type : DataTypes.BOOLEAN,
+                allowNull : false,
+                defaultValue : 0
+            }
+        }, 
+        {
+            
+        }
+    )
+
+    Facture.associate = models => {
+        Facture.belongsTo(models.Devis, { foreignKey : 'idDevis' })
+    }
+
+    return Facture
+}
