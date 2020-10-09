2e1dbccc94282a9a60e240040dcb6ba22cdae2f2
Rémi Courvoisier <remi.courvoisier94@gmail.com>
2020-10-09 16:37:33 +0200

    ajout début front produits. Manque JS

--- public/assets/js/facturation/produitsBusiness.js
+++ public/assets/js/facturation/produitsBusiness.js
@@ -1,3 +1,10 @@
+const BASE_URL = '/facturation/clientsBusiness'
+const SVGPLUS = 'fa-plus'
+const SVGMOINS = 'fa-minus'
+const CREATION = 'Création'
+const MODIFICATION = 'Modification'
+const formAddModify = document.getElementById('formAddModify')
+
 window.addEventListener('load', async () => {
     
     $('.loadingbackground').hide()
