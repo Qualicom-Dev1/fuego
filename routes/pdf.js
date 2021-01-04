b4006ae2da31b7fb4fdce3bb1c7ee7a8cd776b1d
Rémi Courvoisier <remi.courvoisier94@gmail.com>
2021-01-04 15:22:18 +0100

    ajout du logo et de la fiche intervention pour DCME, ne travaillant plus pour Baurenov la fiche d'intervention par défaut devient celle de DCME car plus nombreux

--- routes/pdf.js
+++ routes/pdf.js
@@ -165,7 +165,7 @@ router
     
         let htmlOutput = undefined
     
-        const fiche_intervention = rdv.User !== null ? `${sourcePDFDirectory}/fiche_intervention_${rdv.User.Structures[0].nom}.ejs` : `${sourcePDFDirectory}/fiche_intervention_baurenov.ejs`
+        const fiche_intervention = rdv.User !== null ? `${sourcePDFDirectory}/fiche_intervention_${rdv.User.Structures[0].nom}.ejs` : `${sourcePDFDirectory}/fiche_intervention_DCME.ejs`
     
         ejs.renderFile(fiche_intervention, { layout : false, rdv }, (err, html) => {
             if(err) {
