3358ff04e1bf0ccd62065eea9002ce3da9144d79
Rémi Courvoisier <remi.courvoisier94@gmail.com>
2021-02-03 12:58:37 +0100

    différentes petites modifications de problèmes déjà relevés ou de manques de cohérence. Pour RDV DCME du 03-02-2021

--- public/assets/js/dirco/dirco_rendez_vous.js
+++ public/assets/js/dirco/dirco_rendez_vous.js
@@ -1,3 +1,31 @@
+function setErrorMessage(message) {
+    const div = document.getElementById('div_info')
+    const p = div.getElementsByTagName('p')[0]
+
+    p.classList.add('error_message')
+    p.innerText = message
+    div.style.display = 'block'
+}
+
+function setInformationMessage(message) {
+    const div = document.getElementById('div_info')
+    const p = div.getElementsByTagName('p')[0]
+
+    p.classList.add('info_message')
+    p.innerText = message
+    div.style.display = 'block'
+}
+
+function removeErrorMessage() {
+    const div = document.getElementById('div_info')
+    const p = div.getElementsByTagName('p')[0]
+
+    p.innerText = ''
+    p.classList.remove('error_message')
+    p.classList.remove('info_message')
+    div.style.display = 'none'
+}
+
 $(document).ready(() => {
 
     $('.loadingbackground').hide()
@@ -36,25 +64,80 @@ $(document).ready(() => {
                     $('#modal_liste_RDV').modal({
                         fadeDuration: 100
                     }).ready(() => {
-                        $('.save').click((event) => {
-                            let compteRendu = {
-                                statut: $("input[name=statut]:checked").val(),
-                                idEtat: $("select[name=idEtat]").children("option").filter(":selected").val() == "" ? null : $("select[name=idEtat]").children("option").filter(":selected").val(),
-                                idRdv: $("input[name=idRdv]").val(),
-                                idVendeur: $("select[name=idVendeur]").children("option").filter(":selected").val() == "" ? null : $("select[name=idVendeur]").children("option").filter(":selected").val(),
-                                date: $("input[name=date]").val(),
-                                commentaire: $("input[name=commentaire]").val()
-                            }
+                        setSelectChange()
+                        $('.resultatrdv').click()
+
+                        $('.save').click(async (event) => {
+                            // let compteRendu = {
+                            //     statut: $("input[name=statut]:checked").val(),
+                            //     idEtat: $("select[name=idEtat]").children("option").filter(":selected").val() == "" ? null : $("select[name=idEtat]").children("option").filter(":selected").val(),
+                            //     idRdv: $("input[name=idRdv]").val(),
+                            //     idVendeur: $("select[name=idVendeur]").children("option").filter(":selected").val() == "" ? null : $("select[name=idVendeur]").children("option").filter(":selected").val(),
+                            //     date: $("input[name=date]").val(),
+                            //     commentaire: $("input[name=commentaire]").val()
+                            // }
         
-                            $.ajax({
-                                url: '/manager/update/compte-rendu',
-                                method: 'POST',
-                                data: compteRendu
-                                }).done((data) => {
-                                    window.location.assign('/directeur/rendez-vous')
-                            })
-                            $.modal.close()
+                            // $.ajax({
+                            //     url: '/manager/update/compte-rendu',
+                            //     method: 'POST',
+                            //     data: compteRendu
+                            //     }).done((data) => {
+                            //         window.location.assign('/directeur/rendez-vous')
+                            // })
+                            // $.modal.close()
+
+                            removeErrorMessage()
+
+                            try {
+                                const compteRendu = {
+                                    statut: $("input[name=statut]:checked").val(),
+                                    idEtat: $("select[name=idEtat]").children("option").filter(":selected").val() == "" ? null : $("select[name=idEtat]").children("option").filter(":selected").val(),
+                                    idRdv: $("input[name=idRdv]").val(),
+                                    idVendeur: $("select[name=idVendeur]").children("option").filter(":selected").val() == "" ? null : $("select[name=idVendeur]").children("option").filter(":selected").val(),
+                                    date: $("input[name=date]").val(),
+                                    // commentaire: $("input[name=commentaire]").val(),
+                                    commentaire: $("textarea[name=commentaireRDV]").val(),
+                                    commentaireNew: $("input[name=commentairerepo]").val(),
+                                    datenew: $("input[name=daterepo]").val(),
+                                    rnew: $("input[name=r]").val(),
+                                    sousstatut : $('.traitementactive').html() ? $('.traitementactive').html() : null,
+                                    commentaireHC : $('input[name=commentaireHC]').val(),
+                                    dateRappel : (document.querySelector("input[name=statut]:checked").getAttribute('id') === 'checkarepo') ? ($("input[name=daterappel]").val() !== '' ? $("input[name=daterappel]").val() : undefined) : undefined,
+                                    commentaireRappel : (document.querySelector("input[name=statut]:checked").getAttribute('id') === 'checkarepo') ? ($("input[name=commentaire_rappel]").val() !== '' ? $("input[name=commentaire_rappel]").val() : undefined) : undefined,
+                                    montantVente : $('input[name=montantVente]').val()
+                                }
+
+                                const url = '/manager/update/compte-rendu'
+                                const option = {
+                                    method : 'POST',
+                                    headers : new Headers({
+                                        "Content-type" : "application/json"
+                                    }),
+                                    body : JSON.stringify(compteRendu)
+                                } 
+
+                                const response = await fetch(url, option)
+                                if(!response.ok) throw generalError
+
+                                const data = await response.json()
+                                if(data.infoObject) {
+                                    if(data.infoObject.error) throw data.infoObject.error
+                                    if(data.infoObject.message) setInformationMessage(data.infoObject.message)
+                                }
+
+                                $.modal.close()
+                                window.location.assign('/directeur/rendez-vous')
+                            }
+                            catch(e) {
+                                setErrorMessage(e)
+                            }
                         })
+                        $('.datetimepicker').datetimepicker({
+                            language: 'fr-FR',
+                            allowTimes: [
+                                '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30'
+                            ]
+                        });
                     })
                 })
                 let info = new EJS({ url: '/public/views/partials/traitementclient/info_client'}).render({findedClient: data.findedRdv.Client})
@@ -105,6 +188,65 @@ $(document).ready(() => {
     })
 
 });
+
+function setSelectChange(){
+    $('.resultatrdv').click((element) => {
+        if($('.resultatrdv option:selected').val() == 12 || $('.resultatrdv option:selected').val() == 13 || $('.resultatrdv option:selected').val() == 2){
+            $('.date_repo').show()
+        }else{
+            $('.date_repo').hide()
+        }
+
+        if($('.resultatrdv option:selected').val() == 14) {
+            showHC('compteRendu_HC')            
+        }
+        else if(document.getElementById('div_HC').parentNode.getAttribute('id') === 'compteRendu_HC') {
+            hideHC()            
+        }
+
+        // VENTE
+        if($('.resultatrdv option:selected').val() == 1) {
+            $('#div_Vente').show()
+        }
+        else {
+            $('#div_Vente').hide()
+        }
+    })
+}
+
+function switchSousStatut({ target }) {
+    if(target.classList.contains('traitementactive')) {
+        target.classList.remove('traitementactive')
+    }
+    else {
+        const liste_actifs = document.querySelectorAll('.traitementactive')
+        if(liste_actifs.length > 0) {
+            for(const btn of liste_actifs) {
+                btn.classList.remove('traitementactive')
+            }
+        }
+
+        target.classList.add('traitementactive')
+    }
+}
+
+function showHC(id) {
+    $(`#${id}`).append($('#div_HC'))
+    $('#div_HC').show()
+
+    const liste_btn_traitement = document.getElementsByClassName('btn_traitement')
+    if(liste_btn_traitement.length > 0) {
+        for(const btn of liste_btn_traitement) {
+            btn.onclick = switchSousStatut
+        }
+    }
+}
+
+function hideHC() {
+    $('#div_HC').hide()
+    $('.btn_traitement').removeClass('traitementactive');
+    document.querySelector('#div_HC input[name=commentaireHC]').value = ''
+}
 
 async function openAgency(ids, date) {
     try {
