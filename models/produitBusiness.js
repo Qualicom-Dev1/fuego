65f4a59154f8e8cea93ddf9e650d9e124ac685bd
Rémi Courvoisier <remi.courvoisier94@gmail.com>
2021-02-03 16:43:46 +0100

    augmentation de la désignation d'un produit à 1000 caractères par demande d'Olivier

--- models/produitBusiness.js
+++ models/produitBusiness.js
@@ -15,12 +15,12 @@ module.exports = (sequelize, DataTypes) => {
                 }
             },
             designation : {
-                type : DataTypes.STRING(500),
+                type : DataTypes.STRING(1000),
                 allowNull : true,
                 validate : {
                     len : {
-                        args : [0, 500],
-                        msg : 'La désigantion est limitée à 500 caractères.'
+                        args : [0, 1000],
+                        msg : 'La désigantion est limitée à 1000 caractères.'
                     }
                 }
             },
