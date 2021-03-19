d39e3dedd45d16eea313a6f168504d63dde19c3c
Rémi Courvoisier <remi.courvoisier94@gmail.com>
2021-03-19 12:41:43 +0100

    modification de checkInfosPaiement pour pouvoir vérifier les infos avant que le BDC_client ne soit créé et que depuis le front les infos puissent être vérifiées pour passer à l'étape suivante

--- routes/ADV/bdc_infoPaiements.js
+++ routes/ADV/bdc_infoPaiements.js
@@ -3,12 +3,31 @@ const router = express.Router()
 const models = global.db
 const { ADV_BDC_infoPaiement, ADV_BDC_client } = models
 const { Op } = require('sequelize')
-const { error } = require('winston')
 const errorHandler = require('../utils/errorHandler')
 const isSet = require('../utils/isSet')
 const validations = require('../utils/validations')
 
+// vérifie les infos paiements
 async function checkInfosPaiement(infosPaiement) {
+    if(!isSet(infosPaiement)) throw "Les informations de paiement doivent être transmises."
+    
+    infosPaiement = checkInfosPaiementSent(infosPaiement)
+
+    // vérification du client
+    if(!isSet(infosPaiement.idADV_BDC_client) || isNaN(Number(infosPaiement.idADV_BDC_client))) throw "L'identifiant du client doit être transmis."
+    infosPaiement.idADV_BDC_client = Number(infosPaiement.idADV_BDC_client)
+    const client = ADV_BDC_client.findOne({
+        where : {
+            id : infosPaiement.idADV_BDC_client
+        }
+    })
+    if(client === null) throw "Aucun client correspondant à l'identifiant transmis."    
+
+    return infosPaiement
+}
+
+// vérifie les infos commande telle qu'envoyées (sans l'idADV_BDC_client car pas encore créé)
+function checkInfosPaiementSent(infosPaiement) {
     if(!isSet(infosPaiement)) throw "Les informations de paiement doivent être transmises."
     
     // vérification de l'acompte
@@ -17,8 +36,9 @@ async function checkInfosPaiement(infosPaiement) {
         if(!isSet(infosPaiement.typeAcompte)) throw "Le type d'acompte doit être sélectionné."
         if(!['CHÈQUE', 'ESPÈCES'].includes(infosPaiement.typeAcompte)) throw "Le type d'acompte doit être dans la liste fournie."
         infosPaiement.montantAcompte = validations.validationNumbers(infosPaiement.montantAcompte, "Le montant de l'acompte")
+        if(infosPaiement.montantAcompte <= 0) throw "Le montant de l'acompte doit être positif."
     }
-    else if((!isSet(infosPaiement.isAcompte) || !!!infosPaiement.isAcompte) && (isSet(infosPaiement.typeAcompte) || isSet(infosPaiement.montantAcompte))) throw "Acompte doit être sélectionné." 
+    // else if((!isSet(infosPaiement.isAcompte) || !!!infosPaiement.isAcompte) && (isSet(infosPaiement.typeAcompte) || isSet(infosPaiement.montantAcompte))) throw "Acompte doit être sélectionné." 
     else {
         infosPaiement.isAcompte = false
         infosPaiement.typeAcompte = null
@@ -28,9 +48,9 @@ async function checkInfosPaiement(infosPaiement) {
     // vérification du paiement comptant
     if(isSet(infosPaiement.isComptant) && !!infosPaiement.isComptant) {
         infosPaiement.isComptant = !!infosPaiement.isComptant
-        infosPaiement.montantComptant = validations.validationNumbers(infosPaiement, "Le montant du paiement comptant")
+        infosPaiement.montantComptant = validations.validationPositiveNumbers(infosPaiement.montantComptant, "Le montant du paiement comptant")        
     }
-    else if((!isSet(infosPaiement.isComptant) || !!!infosPaiement.isComptant) && isSet(infosPaiement.montantComptant)) throw "Paiement comptant doit être sélectionné."
+    // else if((!isSet(infosPaiement.isComptant) || !!!infosPaiement.isComptant) && isSet(infosPaiement.montantComptant)) throw "Paiement comptant doit être sélectionné."
     else {
         infosPaiement.isComptant = false
         infosPaiement.montantComptant = 0
@@ -39,22 +59,22 @@ async function checkInfosPaiement(infosPaiement) {
     // vérification du crédit
     if(isSet(infosPaiement.isCredit) && !!infosPaiement.isCredit) {
         infosPaiement.isCredit = !!infosPaiement.isCredit
-        infosPaiement.montantCredit = validations.validationNumbers(infosPaiement.montantCredit, "Le montant du crédit")
-        infosPaiement.nbMensualiteCredit = validations.validationInteger(infosPaiement.nbMensualiteCredit, "Le nombre de mensualités du crédit")
-        infosPaiement.montantMensualiteCredit = validations.validationNumbers(infosPaiement.montantMensualiteCredit, "Le montant des mensualités du crédit")
+        infosPaiement.montantCredit = validations.validationPositiveNumbers(infosPaiement.montantCredit, "Le montant du crédit")
+        infosPaiement.nbMensualiteCredit = validations.validationPositiveInteger(infosPaiement.nbMensualiteCredit, "Le nombre de mensualités du crédit")
+        infosPaiement.montantMensualiteCredit = validations.validationPositiveNumbers(infosPaiement.montantMensualiteCredit, "Le montant des mensualités du crédit")
         infosPaiement.nbMoisReportCredit = validations.validationInteger(infosPaiement.nbMoisReportCredit, "Le nombre de mois de report du crédit")
         infosPaiement.tauxNominalCredit = validations.validationNumbers(infosPaiement.tauxNominalCredit, "Le taux nominal du crédit")
         infosPaiement.tauxEffectifGlobalCredit = validations.validationNumbers(infosPaiement.tauxEffectifGlobalCredit, "Le taux effectif global du crédit")
         infosPaiement.datePremiereEcheanceCredit = validations.validationDateFullFR(infosPaiement.datePremiereEcheanceCredit, "La date de première échéance du crédit")
         infosPaiement.coutTotalCredit = validations.validationNumbers(infosPaiement.coutTotalCredit, "Le coût total du crédit")
     }
-    else if(
-        (!isSet(infosPaiement.isCredit) || !!!infosPaiement.isCredit) && 
-        (
-            isSet(infosPaiement.montantCredit) || isSet(infosPaiement.nbMensualiteCredit) || isSet(infosPaiement.montantMensualiteCredit) || isSet(infosPaiement.nbMoisReportCredit) ||
-            isSet(infosPaiement.tauxNominalCredit) || isSet(infosPaiement.tauxEffectifGlobalCredit) || isSet(infosPaiement.datePremiereEcheanceCredit) || isSet(infosPaiement.coutTotalCredit)
-        )
-    ) throw "Paiement via un organisme de crédit doit être sélectionné."
+    // else if(
+    //     (!isSet(infosPaiement.isCredit) || !!!infosPaiement.isCredit) && 
+    //     (
+    //         isSet(infosPaiement.montantCredit) || isSet(infosPaiement.nbMensualiteCredit) || isSet(infosPaiement.montantMensualiteCredit) || isSet(infosPaiement.nbMoisReportCredit) ||
+    //         isSet(infosPaiement.tauxNominalCredit) || isSet(infosPaiement.tauxEffectifGlobalCredit) || isSet(infosPaiement.datePremiereEcheanceCredit) || isSet(infosPaiement.coutTotalCredit)
+    //     )
+    // ) throw "Paiement via un organisme de crédit doit être sélectionné."
     else {
         infosPaiement.isCredit = false
         infosPaiement.montantCredit = 0
@@ -69,16 +89,6 @@ async function checkInfosPaiement(infosPaiement) {
 
     if((!isSet(infosPaiement.isAcompte) || !!!infosPaiement.isAcompte) && (!isSet(infosPaiement.isComptant) || !!!infosPaiement.isComptant) && (!isSet(infosPaiement.isCredit) || !!!infosPaiement.isCredit))
     throw "Un ou plusieurs modes de réglement doivent être sélectionnés."
-
-    // vérification du client
-    if(!isSet(infosPaiement.idADV_BDC_client) || isNaN(Number(infosPaiement.idADV_BDC_client))) throw "L'identifiant du client doit être transmis."
-    infosPaiement.idADV_BDC_client = Number(infosPaiement.idADV_BDC_client)
-    const client = ADV_BDC_client.findOne({
-        where : {
-            id : infosPaiement.idADV_BDC_client
-        }
-    })
-    if(client === null) throw "Aucun client correspondant à l'identifiant transmis."    
 
     return infosPaiement
 }
@@ -121,11 +131,11 @@ router
     })
 
 })
-.get('/checkInfosPaiement', async (req, res) => {
+.post('/checkInfosPaiement', (req, res) => {
     let infos = undefined
 
     try {
-        await checkInfosPaiement(req.body)
+        checkInfosPaiementSent(req.body)
 
         infos = errorHandler(undefined, 'ok')
     }
@@ -141,5 +151,6 @@ router
 module.exports = {
     router,
     checkInfosPaiement,
+    checkInfosPaiementSent,
     create_BDC_infosPaiement
 }
